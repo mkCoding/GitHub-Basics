cb404fc3aea09987e99b8b0bdedc5a57e8d402f1
Mike Kemakolam <mikekemakolam@MBPM2-HGKM633XMW-KEMAKOLAM.local>
2024-07-01 10:45:54 -0400

    added feature A file

---
+++ app/src/main/java/com/example/github_basics/view/FeatureAScreen.kt
@@ -0,0 +1,10 @@
+package com.example.github_basics.view
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FeatureAScreen() {
+
+    Text(text = "Feature A")
+}
