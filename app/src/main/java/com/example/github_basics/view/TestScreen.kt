ed41bb6580661f2e00f606229282eeb268c45abb
Mike Kemakolam <mikekemakolam@MBPM2-HGKM633XMW-KEMAKOLAM.local>
2024-07-01 09:43:48 -0400

    initial commit

---
+++ app/src/main/java/com/example/github_basics/view/TestScreen.kt
@@ -0,0 +1,36 @@
+package com.example.github_basics.view
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun TestScreen(){
+  Column(
+      modifier = Modifier
+          .fillMaxSize()
+          .padding(16.dp),
+      horizontalAlignment = Alignment.CenterHorizontally
+  ) {
+      Text(
+          text = "hello",
+          style = TextStyle(fontSize = 30.sp)
+      )
+  }
+
+}
+
+
+@Composable
+@Preview (showBackground = true)
+fun PreviewTestScreen(){
+    TestScreen()
+}
