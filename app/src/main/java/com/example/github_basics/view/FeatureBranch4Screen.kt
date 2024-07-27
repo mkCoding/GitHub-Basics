e069069c5152fe170bb9de76762aac73003dc7c2
mkCoding <mkem50@gmail.com>
2024-07-27 14:44:47 -0400

    adding button in Feature 4 to display text

---
+++ app/src/main/java/com/example/github_basics/view/FeatureBranch4Screen.kt
@@ -0,0 +1,50 @@
+package com.example.github_basics.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun FeatureBranch4Screen(){
+
+    //holds text to be displayed
+    var displayText by remember { mutableStateOf("") }
+
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
+        Button(onClick = {
+            displayText = "Wow you just clicked the button"
+        }) {
+            Text(text = "Click Me Now!")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(text = displayText)
+    }
+
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewBranch4Screen(){
+    FeatureBranch4Screen()
+}
