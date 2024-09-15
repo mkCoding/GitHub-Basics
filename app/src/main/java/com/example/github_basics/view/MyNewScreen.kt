c1436032e7975aee2b87f93dc06d01f811abd244
mkCoding <mkem50@gmail.com>
2024-09-15 14:36:59 -0500

    added MyNewScreen

---
+++ app/src/main/java/com/example/github_basics/view/MyNewScreen.kt
@@ -0,0 +1,31 @@
+package com.example.github_basics.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun MyNewScreen(){
+    Column(modifier = Modifier
+        .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Text(text = "Hello World", style = TextStyle(fontSize = 40.sp))
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewMyNewScreen(){
+    MyNewScreen()
+}
