48b2bbdfe99b5cdadf964869441356f8fc26c93a
mkCoding <mkem50@gmail.com>
2024-09-18 23:58:49 -0500

    adding almost friday

--- app/src/main/java/com/example/github_basics/view/Practice Screen.kt
+++ app/src/main/java/com/example/github_basics/view/Practice Screen.kt
@@ -69,7 +69,7 @@ fun MyCard() {
                 style = TextStyle(fontSize = 30.sp, fontWeight = FontWeight.Bold)
             )
             Text(
-                text = "Its Thursday!!!",
+                text = "Its Almost Friday!!!",
                 style = TextStyle(fontSize = 30.sp, fontWeight = FontWeight.Bold)
             )
 
