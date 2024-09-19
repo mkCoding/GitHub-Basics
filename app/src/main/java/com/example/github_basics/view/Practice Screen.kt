ac70704c16f41963cf138eb80f50cb17b8060709
mkCoding <mkem50@gmail.com>
2024-09-18 23:11:05 -0500

    added card to PracticeScreen

--- app/src/main/java/com/example/github_basics/view/Practice Screen.kt
+++ app/src/main/java/com/example/github_basics/view/Practice Screen.kt
@@ -1,19 +1,31 @@
 package com.example.github_basics.view
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun PracticeScreen(){
     Column(
-        modifier = Modifier.fillMaxSize().padding(16.dp),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(text = "Practice Now!")
@@ -34,6 +46,30 @@ fun PracticeScreen(){
         Text(text = "Jack Jumping!")
         Text(text = "Daisy Flower!")
 
+        MyCard()
+
+    }
+}
+
+@Composable
+fun MyCard(){
+    Card(
+        colors = CardDefaults.cardColors(Color.Red),
+        modifier = Modifier
+            .padding(top = 40.dp)
+            .width(400.dp)
+            .height(300.dp)
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "This is my Card",
+                style = TextStyle(fontSize = 40.sp, fontWeight = FontWeight.Bold))
+
+        }
     }
 }
 
